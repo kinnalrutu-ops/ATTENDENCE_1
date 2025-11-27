73653522cfb8c25be5e42332dd63771c9b276e16
Rutu <kinnalrutu@gmail.com>
2025-11-27 15:05:18 +0530

    Update STUDENT_ATTENDENCE.py

--- STUDENT_ATTENDENCE.py
+++ STUDENT_ATTENDENCE.py
@@ -1,14 +1,11 @@
-# Student Attendance Tracker Program with Eligibility Check
-
-classes_held = int(input("Enter number of classes held: "))
-classes_attended = int(input("Enter number of classes attended: "))
-
+import sys
+if sys.argv==2:
+    classes_held=sys.argv[1]
+    classes_attended=sys.argv[2]
 attendance = (classes_attended / classes_held) * 100
-
 print("Classes Held:", classes_held)
 print("Classes Attended:", classes_attended)
 print("Attendance:", round(attendance, 2), "%")
-
 if attendance >= 75:
     print("Status: Eligible for exams")
 else:
